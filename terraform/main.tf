fb77f90ec15b079d48985f52f53874827f4b0674
HazemFelhi <hazemfelhi99@gmail.com>
2024-03-21 02:41:17 +0100

    fix the infrastructure using terraform

---
+++ terraform/main.tf
@@ -0,0 +1,86 @@
+resource "aws_vpc" "mtc_vpc" {
+  cidr_block           = "10.123.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    name = "dev"
+  }
+}
+
+resource "aws_subnet" "mtc_public_subnet" {
+  vpc_id                  = aws_vpc.mtc_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "eu-north-1a"
+
+  tags = {
+    Name = "dev_public"
+  }
+}
+
+resource "aws_internet_gateway" "mtc_internet_gateway" {
+  vpc_id = aws_vpc.mtc_vpc.id
+
+  tags = {
+    Name = "dev_igw"
+  }
+}
+
+resource "aws_route_table" "mtc_public_rt" {
+  vpc_id = aws_vpc.mtc_vpc.id
+
+  tags = {
+    Name = "dev_public_rt"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id            = aws_route_table.mtc_public_rt.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.mtc_internet_gateway.id
+}
+
+resource "aws_route_table_association" "mtc_public_assoc" {
+  subnet_id      = aws_subnet.mtc_public_subnet.id
+  route_table_id = aws_route_table.mtc_public_rt.id
+}
+
+resource "aws_security_group" "mtc_sg" {
+  name        = "dev_sg"
+  description = "dev srecurity group"
+  vpc_id      = aws_vpc.mtc_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "mtc_auth" {
+  key_name           = "mtc_key"
+  public_key = file("~/.ssh/mtckey.pub")
+}
+
+resource "aws_instance" "jenkins" {
+  ami = "ami-0914547665e6a707c" #data.aws_ami.server_ami.id
+  instance_type = "t3.micro"
+  key_name = aws_key_pair.mtc_auth.key_name
+  vpc_security_group_ids = [aws_security_group.mtc_sg.id]
+  subnet_id = aws_subnet.mtc_public_subnet.id
+  user_data = file("userdata.tpl")
+  tags = {
+    Name = "jenkins_server"
+  }
+
+
+}
